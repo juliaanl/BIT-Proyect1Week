304db3f14834ba7d793e6dfc2d2d16c59d6a810d
juliaanl <julianrincon520@gmail.com>
2021-09-22 16:45:50 -0500

    PrimerCommit

---
+++ JS/Ejercicios.js
@@ -0,0 +1,30 @@
+/*alert("¡Hey! ¡Hola! ¡Salut! ¡Olá! ¡Hej! ¡Hallo! ¡Ciao!");
+console.log("¡Hola!")
+prompt("Ingresa tu nombre")
+confirm("¿Seguro quieres abandonar esta página?")*/
+
+const nombreAlbum1 = () => {
+
+    alert("THE FACE EP");
+}
+
+const nombreAlbum2 = () => {
+
+    alert("SETTLE");
+}
+const nombreAlbum3 = () => {
+
+    alert("THE SINGLES-EP");
+}
+const nombreAlbum4 = () => {
+
+    alert("CARACAL");
+}
+const nombreAlbum5 = () => {
+
+    alert("MOOG FOR LOVE-EP");
+}
+const nombreAlbum6 = () => {
+
+    alert("ENERGY");
+}
